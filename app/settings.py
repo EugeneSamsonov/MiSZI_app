e13beae8362a915cc967f701d9227053fd75510d
EugeneSamsonov <samsonovevgenii231@gmail.com>
2025-10-07 19:50:48 +0300

    refactor css: add static

--- app/settings.py
+++ app/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static", # Or specify the path to your app's static folder
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
